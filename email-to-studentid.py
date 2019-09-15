f7b2acdf0ae778fc1c25cb1f116b10b50cc15ccb
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2019-09-15 13:37:13 +0300

    Convert "None" string to 0.0.

--- email-to-studentid.py
+++ email-to-studentid.py
@@ -15,11 +15,14 @@ def remove_emails(osallistujat):
     for line in sys.stdin:
         line = line.rstrip('\r\n')
         a = line.split(";")
+
+        if a[1] == 'None':
+            a[1] = '0.0'
+
         if a[0] in osallistujat:
-#        if osallistujat.has_key(a[0]):
             print(osallistujat[a[0]] + ";" + a[1])
         else:
-            print(line)
+            print(a[0] + ";" + a[1])
 
 osallistujat = read_osallistujat(sys.argv[1])
 #print(osallistujat)
